6a15b9496ddc06ab3353b33c7e54a1708b8848a7
Marcus Ficner <mail@marcusficner.de>
2015-01-14 21:13:21 +0100

    Initial Commit

---
+++ HeadspaceDot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeadspaceDot
+//
+//  Created by Marcus Ficner on 14/01/15.
+//  Copyright (c) 2015 Marcus Ficner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
